64c7725906f5a98d06cf5c56100c37ec40a32dd6
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-04-01 08:20:24 +0100

    added task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,17 +2,36 @@
 'Writing strings to Redis'
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
     '''Creates a Class Cache that writes a string to redis'''
+
     def __init__(self):
+        '''Initializes the Cache Class'''
         self._redis = redis.Redis()
 
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''Stores data to redis alongsides a key'''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        if not self.exists(key):
+            return None
+        value = self._redis.get(key)
+
+        if fn:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str):
+        return self.get(key, fn=lambda x: x.decode())
+
+    def get_int(self, key: str):
+        return self.get(key, fn=lambda x: int(x))
